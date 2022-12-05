1824a15d026af79b81edfbb2b8201b5d0ab85132
Spola1 <1111.22224@mail.ru>
2022-12-05 17:07:37 +0300

    task 7

---
+++ task7/07.rb
@@ -0,0 +1,16 @@
+def expanded_size(code)
+  size = 0
+  if code =~ /\(((\d+)x(\d+))\)/
+    size += $`.size
+    to_decode = $'[0...$2.to_i]
+    size += $3.to_i * expanded_size(to_decode) + expanded_size($'[$2.to_i..-1])
+  else
+    code.size
+  end
+end
+
+File.read('07.txt').each_line do |line|
+  line.chomp!
+
+  puts expanded_size(line)
+end
